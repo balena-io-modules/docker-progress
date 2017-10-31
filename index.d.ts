8b0c3be5f82c58ee08ee94ce5b063b29fa3d4977
Cameron Diver <cameron@resin.io>
2017-10-31 16:21:03 +0100

    Add typings for index.coffee
    Change-type: minor
    Connects-to: #36
    Signed-off-by: Cameron Diver <cameron@resin.io>

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+declare module 'docker-progress' {
+
+	type ProgressCallback = (progressObj: any) => void;
+
+	class DockerProgress {
+
+		public constructor(opts?: any);
+
+		public aggregateProgress(count: number, onProgress: ProgressCallback): ProgressCallback[];
+
+		public pull(
+			image: string,
+			onProgress: ProgressCallback,
+			options: any,
+			callback?: ((err: Error, data: any) => void),
+		): Promise<any>;
+
+		public push(
+			image: string,
+			onProgress: ProgressCallback,
+			options: any,
+			callback?: ((err: Error, data: any) => void),
+		): Promise<any>;
+
+	}
+}
